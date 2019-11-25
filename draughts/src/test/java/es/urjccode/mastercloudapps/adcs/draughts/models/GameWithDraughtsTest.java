c689a2a65c0f4b4124dc8f4851a3d066c52b2d26
Javier Vela <fvelaayl@everis.com>
2019-11-25 11:08:10 +0100

    tdd test to check can't move over same color -fail

--- draughts/src/test/java/es/urjccode/mastercloudapps/adcs/draughts/models/GameWithDraughtsTest.java
+++ draughts/src/test/java/es/urjccode/mastercloudapps/adcs/draughts/models/GameWithDraughtsTest.java
@@ -1,5 +1,6 @@
 package es.urjccode.mastercloudapps.adcs.draughts.models;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
@@ -68,7 +69,7 @@ public class GameWithDraughtsTest {
     }
 
     @Test 
-    public void testGivenGameWhenWhiteDraughtsMove() {
+    public void testGivenGameWhenWhiteDraughtsThenMove() {
         Coordinate origin = new Coordinate(0, 1);
 
         Coordinate target = new Coordinate(1, 0);
@@ -83,7 +84,7 @@ public class GameWithDraughtsTest {
     }
 
     @Test 
-    public void testGivenGameWhenBlackDraughtsMove() {
+    public void testGivenGameWhenBlackDraughtsThenMove() {
         Coordinate origin = new Coordinate(7,2);
         Coordinate target = new Coordinate(6,3);
 
@@ -96,5 +97,42 @@ public class GameWithDraughtsTest {
         Piece draught = game.getPiece(target);
         assertNull(game.getPiece(origin));
         assertTrue(draught instanceof Draught);  
+    }
+
+    @Test 
+    public void testGivenGameWhenBlackDraughtsMoveOverPieceThenCapturePiece() {
+        Coordinate origin = new Coordinate(7,2);
+        Coordinate target = new Coordinate(5,4);
+        Coordinate pieceCaptured = new Coordinate(6, 3);
+
+        when(turn.getColor()).thenReturn(Color.BLACK);
+
+        Game game = new GameBuilder(turn).addRow(7, "  N     ")
+            .addRow(6,"   b    " ).build();
+
+        game.move(origin, target);
+        
+        Piece draught = game.getPiece(target);
+        assertNull(game.getPiece(origin));
+        assertNull(game.getPiece(pieceCaptured));
+        assertTrue(draught instanceof Draught);  
+    }
+
+    @Test 
+    public void testGivenGameWhenWhiteDraughtsMoveOverPieceSameColorThenError() {
+        Coordinate origin = new Coordinate(0, 1);
+        Coordinate target = new Coordinate(2, 3);
+
+        Game game = new GameBuilder()
+            .addRow(0, " B      ")
+            .addRow(1, "  b     ").build();
+
+       assertEquals(Error.EATING_SAME_COLOR, game.isCorrect(origin, target));
+
+        // game.move(origin, target);
+
+        // Piece draught = game.getPiece(target);
+        // assertNull(game.getPiece(origin));
+        // assertTrue(draught instanceof Draught);  
     }
 }
